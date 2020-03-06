2af445f370000f08e4d6cbeaf5e326496cff8c92
Guilherme de Favere <guilherme.favere@gmail.com>
2020-03-05 22:10:40 -0300

    First commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const App: () => React$Node = () => {
+  return (
+    <View>
+      <Text>Rocket shoes</Text>
+    </View>
+  );
+};
+
+export default App;
