be41cf5475e8ef31c3ad17b8570b3c6f2ddd7f74
guifavere <guilherme.favere@gmail.com>
2020-05-05 21:34:20 -0300

    Create products list at home page

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,6 +1,40 @@
 import React from 'react';
-import { Text } from 'react-native';
+
+import {
+  Product,
+  ProductList,
+  Image,
+  Price,
+  Title,
+  Button,
+  Quantity,
+  Label,
+} from './styles';
 
 export default function Main() {
-  return <Text>Main page</Text>;
+  const products = [1, 2, 3, 4, 5];
+
+  return (
+    <ProductList
+      data={products}
+      horizontal
+      keyExtractor={item => item}
+      renderItem={() => (
+        <Product>
+          <Image
+            source={{
+              uri:
+                'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis2.jpg',
+            }}
+          />
+          <Title>Tênis de caminhada leve confortável</Title>
+          <Price>R$ 179,90</Price>
+          <Button>
+            <Quantity>1</Quantity>
+            <Label>Adicionar</Label>
+          </Button>
+        </Product>
+      )}
+    />
+  );
 }
---
+++ src/pages/Main/styles.js
@@ -0,0 +1,68 @@
+import styled from 'styled-components/native';
+import { darken } from 'polished';
+
+export const ProductList = styled.FlatList`
+  margin-top: 20px;
+`;
+
+export const Product = styled.TouchableOpacity`
+  background: #fff;
+  border-radius: 4px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  height: 358px;
+  margin: 0 20px 20px;
+  padding: 10px;
+  width: 220px;
+`;
+
+export const Image = styled.Image`
+  background: #ececec;
+  height: 200px;
+  margin-bottom: 10px;
+  width: 200px;
+`;
+
+export const Title = styled.Text`
+  color: #333;
+  font-family: Roboto;
+  font-size: 16px;
+  line-height: 21px;
+`;
+
+export const Price = styled.Text`
+  color: #000;
+  font-family: Roboto;
+  font-size: 21px;
+  font-weight: bold;
+  line-height: 25px;
+  margin: 5px 0 14px;
+`;
+
+export const Button = styled.TouchableOpacity`
+  align-items: stretch;
+  border-radius: 4px;
+  border: 1px solid green;
+  flex-direction: row;
+  height: 42px;
+`;
+
+export const Quantity = styled.Text`
+  background: ${darken(0.1, '#7159c1')};
+  color: #fff;
+  line-height: 40px;
+  padding-right: 12px;
+  text-align: right;
+  width: 53px;
+`;
+
+export const Label = styled.Text`
+  background: #7159c1;
+  color: #fff;
+  flex-grow: 1;
+  font-family: Roboto;
+  font-size: 14px;
+  font-weight: bold;
+  line-height: 40px;
+  text-align: center;
+  text-transform: uppercase;
+`;
